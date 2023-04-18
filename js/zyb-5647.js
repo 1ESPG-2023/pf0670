f85e6c42bd5f6d9d166372b19db0f6cff8f067d6
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-04-18 10:58:06 -0300

    spread + html collections + map

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,23 +1,54 @@
+ 
+ 
+// // for (let index = 0; index < liElement.length ; index++) {
+// //     console.log(liElement[index].textContent)
+// // }
+// // // console.log(liElement.length)
 
-const liElement = document.getElementsByTagName("li");
-  
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // // let nr3 = nr1+nr2;
+// // let nr3 = [nr1,nr2];
 
-// for (let index = 0; index < liElement.length ; index++) {
-//     console.log(liElement[index].textContent)
-// }
-// // console.log(liElement.length)
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
 
-//Declaração de arrays
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-// let nr3 = nr1+nr2;
-let nr3 = [nr1,nr2];
+// // //Listando um array! Técnica especial
+// // // nr3.forEach((itemDoaArray)=>{
+// // //     console.log(`Array-3 sendo listado ${itemDoaArray}`);
+// // // })
+// // // Listando o array com MAP
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
+// //     numero.forEach((digitos)=>{
+// //         console.log(digitos)
+// //     })
+// // })
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // //Espalhar o array com SPREAD [ ... ]
+// // let nr3 = [...nr1,...nr2];
 
-//Listando um array! Técnica especial
-nr3.forEach((itemDoaArray)=>{
-    console.log(`Array-3 sendo listado ${itemDoaArray}`);
-})
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
+
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
+
+// // })
+
+// //Convertendo HTMLCollection em Array de forma indireta
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHtml];
+
+// liElementArray.map((item,key)=>{
+//     if (item.textContent == "Item-39") {
+//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
+// })
+
